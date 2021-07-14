845806767ddaef52de4e5e090f8c57d2140317b1
leonace924 <meobius123@gmail.com>
2021-07-14 11:26:05 +0300

    Add static content separate

---
+++ src/utils/textContent.js
@@ -0,0 +1,11 @@
+const content = {
+  hero: {
+    backgroundImage: './images/img-hero.jpg',
+    title: 'We produce your experiences professionally, on time, and with a little sizzle.',
+    description: 'Custom solutions with a 1-team per client approach, 24/7.',
+    ctaLabel: 'Get Started',
+    ctaLink: '/',
+  },
+};
+
+export default content;
